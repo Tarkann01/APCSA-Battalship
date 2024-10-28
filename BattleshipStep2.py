a36c6c45392d6a54358fc43267018a9f43714cfd
MossBomb <coolfam99@outlook.com>
2024-10-28 10:00:03 -0700

    Add files via upload
    Added step 2 to python code and fixed 2d array

---
+++ BattleshipStep2.py
@@ -0,0 +1,17 @@
+ocean = [
+    ["O", "O", "O", "O", "O"],
+    ["O", "O", "O", "O", "O"],
+    ["O", "O", "O", "O", "O"],
+    ["O", "O", "O", "O", "O"],
+    ["O", "O", "O", "O", "O"],
+]
+
+
+
+def startTurn():
+    for x in range (0, 5):
+        print(ocean[x])
+
+
+startTurn()
+
