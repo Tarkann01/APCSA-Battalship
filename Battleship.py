91c88820edee069298d8a147bb85dae1944b17fb
MossBomb <coolfam99@outlook.com>
2024-10-30 08:44:03 -0700

    Add files via upload
    Combined features from testing branch and step 8 while adding some stuff from step 12

--- Battleship.py
+++ Battleship.py
@@ -6,22 +6,25 @@ shotCount = 0
 
 ocean = [[]]
 
-oceanRows = 0
-oceanColumns = 0
+oceanRC = 0
+totalShips = 1
+shipsHit = 0
 
 def createOcean():
     global ocean
-    global oceanRows
-    global oceanColumns
-    oceanRows = int(input("How many rows do you want?"))
-    oceanColumns = int(input("How many columns do you want?"))
-    ocean = [["O" for _ in range(oceanColumns)] for _ in range(oceanRows)]
+    global oceanRC
+    oceanRC = int(input("How many rows and columns do you want?"))
+    #oceanColumns = int(input("How many columns do you want?"))
+     #Sadly I had to comment out this an give the user only 1 input due to step 12b, as after asking the teacher the user
+    #Is only supposed to have one input for rows and columns.
+
+    ocean = [["O" for _ in range(oceanRC)] for _ in range(oceanRC)]
 
 def startTurn():
     xGuess = -1
     yGuess = -1
     global shotCount
-    for z in range (0, 5):
+    for z in range (0, oceanRC):
         print(ocean[z])
 
     #TESTING PURPOSES
@@ -29,31 +32,43 @@ def startTurn():
     print(y)
     #TESTING PURPOSES
 
-    while xGuess < 0 or xGuess > oceanRows:
+    while xGuess < 0 or xGuess > oceanRC:
         xGuess = int(input("Please input the x position of where you believe the ship is."))
-        if xGuess > oceanRows or xGuess < 0:
+        if xGuess > oceanRC or xGuess < 1:
             print("Guess is out of bounds, try again")
-    while yGuess < 0 or yGuess > oceanColumns:
+    while yGuess < 0 or yGuess > oceanRC:
         yGuess = int(input("Please input the y position of where you believe the ship is."))
-        if yGuess > oceanColumns or yGuess < 0:
+        if yGuess > oceanRC or yGuess < 1:
             print("Guess is out of bounds, try again")
 
     if x == xGuess and y == yGuess:
         global terminate
+        global shipsHit
+        global totalShips
+        shipsHit += 1
         print("You guessed Correctly!")
-        terminate = 1
+        print("Shot Count:",shotCount)
+        print("Ships Hit:",shipsHit)
+        if totalShips == shipsHit:
+            print("You Win!!")
+            terminate = 1
+
     else:
         print("WRONG! Try again!")
-        ocean[xGuess-1][yGuess-1] = "X"
+        ocean[yGuess-1][xGuess-1] = "X"
         shotCount +=1
         print("Shot Count:",shotCount)
+        print("Ships Hit:",shipsHit)
+        if shotCount == oceanRC + totalShips:
+            print("You guessed too many times. You lose!")
+            terminate = 1
 
 
 def hideBoat():
     global x
     global y
-    x = random.randint(0,4)
-    y = random.randint(0,4)
+    x = random.randint(1,oceanRC)
+    y = random.randint(1,oceanRC)
     #ocean.insert("X", ocean[y,x])
 
 createOcean()
